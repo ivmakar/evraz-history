80eb24390384b5e07a2addbbe3d77ed87903d800
ivmakar <ivan.r.makarov@yandex.ru>
2020-05-18 13:32:43 +0300

    ADD -- start activity

---
+++ app/src/main/java/com/example/evraz_history/MenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.evraz_history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
