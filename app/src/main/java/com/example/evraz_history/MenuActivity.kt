0ec15a9b5a92bd09e9f6745ed06bd7ddd6eaa2bb
ivmakar <ivan.r.makarov@yandex.ru>
2020-05-18 15:08:24 +0300

    ADD -- menu activity

--- app/src/main/java/com/example/evraz_history/MenuActivity.kt
+++ app/src/main/java/com/example/evraz_history/MenuActivity.kt
@@ -1,12 +1,26 @@
 package com.example.evraz_history
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        menu1.setOnClickListener {
+            startActivity(Intent(this, HistoryActivity::class.java))
+        }
+
+        menu2.setOnClickListener {
+            startActivity(Intent(this, HistoryActivity::class.java))
+        }
+
+        menu3.setOnClickListener {
+            startActivity(Intent(this, HistoryActivity::class.java))
+        }
     }
 }
