6deb3c700583b5f41688e1d15c4545607999dbc2
ivmakar <ivan.r.makarov@yandex.ru>
2020-05-18 12:43:25 +0300

    init commit

---
+++ app/src/main/java/com/example/evraz_history/MainActivity.kt
@@ -0,0 +1,12 @@
+cd package com.example.evraz_history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
